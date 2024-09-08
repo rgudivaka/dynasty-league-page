18706d76d543e1a8319613f896fd2e1f2261ec62
rgudivaka <rishi.gudivaka@gmail.com>
2024-09-08 15:45:10 -0700

    change footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,59 +1,105 @@
 <script>
-	import { goto } from '$app/navigation';
-    import { managers } from '$lib/utils/helper';
-	import { tabs } from '$lib/utils/tabs';
-	import { onMount } from 'svelte';
+	import { goto } from "$app/navigation";
+	import { managers } from "$lib/utils/helper";
+	import { tabs } from "$lib/utils/tabs";
+	import { onMount } from "svelte";
 
 	let outOfDate = false;
 
-    let el, footerHeight;
+	let el, footerHeight;
 
-    let innerWidth;
+	let innerWidth;
 
-    const resize = (e, delay) => {
-        const bottom = el?.getBoundingClientRect().bottom;
-        const top = el?.getBoundingClientRect().top;
-        if(delay) {
-            setTimeout(() => {
-                resize(e, false);
-            }, 100)
-        } else {
-            footerHeight = bottom - top;
-        }
-    }
+	const resize = (e, delay) => {
+		const bottom = el?.getBoundingClientRect().bottom;
+		const top = el?.getBoundingClientRect().top;
+		if (delay) {
+			setTimeout(() => {
+				resize(e, false);
+			}, 100);
+		} else {
+			footerHeight = bottom - top;
+		}
+	};
 
 	onMount(async () => {
-		const res = await fetch('/api/checkVersion', {compress: true})
+		const res = await fetch("/api/checkVersion", { compress: true });
 		const needUpdate = await res.json();
 		outOfDate = needUpdate;
-        resize(el?.getBoundingClientRect(), true);
-	})
+		resize(el?.getBoundingClientRect(), true);
+	});
 
-    let managersOutOfDate = false;
-    if(managers) {
-        for(const manager of managers) {
-            if(manager.roster && !manager.managerID) {
-                managersOutOfDate = true;
-                resize(el?.getBoundingClientRect(), true);
-                break;
-            }
-        }
-    }
+	let managersOutOfDate = false;
+	if (managers) {
+		for (const manager of managers) {
+			if (manager.roster && !manager.managerID) {
+				managersOutOfDate = true;
+				resize(el?.getBoundingClientRect(), true);
+				break;
+			}
+		}
+	}
 
 	const year = new Date().getFullYear();
 
-    $: resize(el?.getBoundingClientRect(), false, innerWidth);
+	$: resize(el?.getBoundingClientRect(), false, innerWidth);
 </script>
 
-<svelte:window bind:innerWidth={innerWidth} />
+<svelte:window bind:innerWidth />
+
+<div class="footerSpacer" style="height: {footerHeight}px;" />
+
+<!-- footer with update notice -->
+<footer bind:this={el}>
+	{#if outOfDate}
+		<p class="updateNotice">
+			There is an update available for your League Page. <a
+				href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iv-updates"
+				>Follow the Update Instructions</a
+			> to get all of the newest features!
+		</p>
+	{/if}
+	{#if managersOutOfDate}
+		<p class="updateNotice">
+			Your managers page needs an update, <a
+				href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#2-add-managers"
+				>please follow the instructions</a
+			> to get the most up-to-date experience.
+		</p>
+	{/if}
+	<div id="navigation">
+		<ul>
+			{#each tabs as tab}
+				{#if !tab.nest}
+					<li>
+						<div class="navLink" on:click={() => goto(tab.dest)}>
+							{tab.label}
+						</div>
+					</li>
+				{:else}
+					{#each tab.children as child}
+						<!-- Shouldn't show Managers tab unless managers has been populated -->
+						{#if child.label != "Managers" || managers.length > 0}
+							<li>
+								<div class="navLink" on:click={() => goto(child.dest)}>
+									{child.label}
+								</div>
+							</li>
+						{/if}
+					{/each}
+				{/if}
+			{/each}
+		</ul>
+	</div>
+</footer>
 
 <style>
 	footer {
 		background-color: var(--f8f8f8);
 		width: 100%;
-        display: block;
-        position: absolute;
-        bottom: 0;
+		display: block;
+		position: absolute;
+		bottom: 0;
 		z-index: 1;
 		border-top: 1px solid #920505;
 		padding: 30px 0 60px;
@@ -96,39 +142,3 @@
 		margin-top: 0;
 	}
 </style>
-
-<div class="footerSpacer" style="height: {footerHeight}px;" />
-
-<!-- footer with update notice -->
-<footer bind:this={el}>
-    {#if outOfDate}
-	    <p class="updateNotice">There is an update available for your League Page. <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iv-updates">Follow the Update Instructions</a> to get all of the newest features!</p>
-    {/if}
-    {#if managersOutOfDate}
-	    <p class="updateNotice">Your managers page needs an update, <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#2-add-managers">please follow the instructions</a> to get the most up-to-date experience.</p>
-    {/if}
-	<div id="navigation">
-		<ul>
-			{#each tabs as tab}
-				{#if !tab.nest}
-					<li><div class="navLink" on:click={() => goto(tab.dest)}>{tab.label}</div></li>
-				{:else}
-					{#each tab.children as child}
-                        <!-- Shouldn't show Managers tab unless managers has been populated -->
-				        {#if child.label != "Managers" || managers.length > 0}
-                            <li><div class="navLink" on:click={() => goto(child.dest)}>{child.label}</div></li>
-                        {/if}
-					{/each}
-				{/if}
-			{/each}
-		</ul>
-	</div>
-	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
-	<br />
-	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
-	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
-</footer>
